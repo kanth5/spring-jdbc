2d7f7d656177449f42978f3fecfe27a3cc944251
kanth5 <mrajk5@gmail.com>
2024-01-09 20:12:01 +0530

    SELECT all and SELECT one from a table

---
+++ tennis-player/src/main/java/com/springdata/tennisplayer/PlayerDao.java
@@ -0,0 +1,24 @@
+package com.springdata.tennisplayer;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PlayerDao {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Player> getAllPlayers() {
+        String sql = "SELECT * FROM PLAYER";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Player.class));
+    }
+
+    public Player getPlayerById(int id) {
+        String sql = "SELECT * FROM PLAYER WHERE ID = ?";
+        return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Player.class), id);
+    }
+}
